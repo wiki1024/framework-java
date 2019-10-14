cfaeabdeb507dd186c3c41bc2b0483cc28b6c706
wiki <weiji.chen@mobilemd.cn>
2019-10-14 14:58:28 +0800

    remove test

--- framework-system-context/src/test/java/com/wiki/framework/system/context/mybatis/Listener/mapper/SubjectMapper.java
+++
@@ -1,7 +0,0 @@
-package com.wiki.framework.system.context.mybatis.Listener.mapper;
-
-import com.wiki.framework.mybatis.mybatis.BaseMapper;
-import com.wiki.framework.system.context.mybatis.Listener.po.Subject;
-
-public interface SubjectMapper extends BaseMapper<Subject> {
-}
